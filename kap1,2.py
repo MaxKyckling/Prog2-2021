1c511166081e1d8e2a556c0e3f51ed1b158f28b3
MaxKyckling <maxostlund03@gmail.com>
2021-09-02 08:46:52 +0200

    kattisstuckintimeloop

---
+++ kap1,2.py
@@ -0,0 +1,7 @@
+#Gör ett program som skriver ut alla tal under 1000 som är jämnt delbara med 7
+for n in range(1,1001):
+    quota = n/7
+    if(quota == int(n/7)):
+        print(n)
+
+#Gör ett program som räknar antalet siffror i en godtycklig inmatad textsträng
---
+++ kattisStuckInATimeLoop.py
@@ -0,0 +1,12 @@
+print("mehouhduehehahahahahaha yeehaw i shall break your looop haw yee")
+try:
+    n = int(input("Ange trollkarlens magiska tal yeehaw "))
+    if(n < 1):
+        print("Talet måste vara mellan 1 och 100 haw yee")
+    elif(n > 100):
+        print("Talet måste vara mellan 1 och 100 yee hew")
+    else:
+        for i in range(1,n+1):
+            print(i, "Abrakadabra")
+except:
+    print("abrakadabra du är en idiot, ge mig något bra inte ditt fucking skrot")

416ca470a34c6361ff16f4358114928d073dc25f
MaxKyckling <maxostlund03@gmail.com>
2021-09-30 08:39:23 +0200

    gjort ett program som printar ut sicksackmönster men det funkar inte ännu

--- ladder.py
+++ ladder.py
@@ -2,17 +2,33 @@
 
 import time
 
-ladder = "\\"
-space = " "
 
-for n in range(1, 10):
-    print(ladder)
-    ladder = space + ladder
-    time.sleep(0.1)
+switches = int(input("Hur många gånger kommer den byta håll? "))
 
-ladder = "/"
+loops = int(input("Hur lång ska den bli? "))
 
-for n in range(1, 10):
-    print(ladder)
-    ladder = space + ladder
-    time.sleep(0.1)
+def drawRight(loops, iSpace):
+    ladder = "\\"
+    space = " "
+    for n in range(1, loops):
+        print(iSpace + ladder)
+        iSpace = iSpace + space
+        time.sleep(0.05)
+    return iSpace
+
+def drawLeft(loops, iSpace):
+    space = " "
+    ladder = "/"
+
+    for n in range(1, loops):
+        iSpace = iSpace[1::1]
+        print(iSpace + ladder)
+        time.sleep(0.05)
+    return iSpace
+
+for i in(1, switches):
+    print(i)
+    #iSpace = drawRight(loops, "")
+
+    #iSpace = drawLeft(loops, iSpace)
+

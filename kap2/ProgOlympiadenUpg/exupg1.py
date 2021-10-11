ee114fe7abb0097dd0e06ccdd9d6d2d8583f59b4
MaxKyckling <maxostlund03@gmail.com>
2021-10-11 15:21:08 +0200

    Hittade inte programmeringsolympiad upgifter men det stod att de läste uppgifter på samma sätt som i kattis, så jag lärde mig hur man gör där och jag submittade BijelBijele, StuckInATimeLoop, Hello world och Buka

---
+++ kap2/ProgOlympiadenUpg/exupg1.py
@@ -0,0 +1,8 @@
+from sys import stdin
+tokens = stdin.readline().split()
+a1 = int(tokens[0])
+a2 = int(tokens[1])
+tokens = stdin.readline().split()
+d = float(tokens[0])
+word = tokens[1]
+print(word)

0a5f8974c3b45802c05f472a1a3f8ae584c0a21b
MaxKyckling <maxostlund03@gmail.com>
2021-12-16 09:41:24 +0100

    boll

--- tkinter/pong.py
+++ tkinter/pong.py
@@ -5,12 +5,11 @@ import math
 root = Tk()
 canvasWidth = 1000
 canvasHeight = 800
-canvas = Canvas(root,height=canvasHeight,width=canvasWidth,bg="#fff")
+canvas = Canvas(root,height=canvasHeight,width=canvasWidth,bg="#000000")
 canvas.pack()
-antSpeed = 1.5
 
 class Ball:
-    def __init__(self,canvas, x, y, diameter, color, xVelocity, yVelocity):
+    def __init__(self,canvas, x, y, diameter, color, xVelocity, yVelocity, obstacles):
         self.canvas = canvas
         self.x = x
         self.y = y
@@ -18,7 +17,46 @@ class Ball:
         self.color = color
         self.xVelocity = xVelocity
         self.yVelocity = yVelocity
-        self.image = canvas.create_oval(x,y,diameter,diameter,fill=color)
+        self.obstacles = obstacles
+        self.image = canvas.create_oval(x,y,x+diameter,y+diameter,fill=color)
+    
+    def move(self):
+        
+        coordinates = self.canvas.coords(self.image)
+        if(coordinates[2]>=(self.canvas.winfo_width()) or coordinates[0]<0):
+            self.xVelocity = -self.xVelocity
+        if(coordinates[3]>=(self.canvas.winfo_height()) or coordinates[1]<0):
+            self.yVelocity = -self.yVelocity
+        self.canvas.move(self.image,self.xVelocity,self.yVelocity)
+        #print(coordinates[0],coordinates[1],coordinates[2],coordinates[3])
+        for i in range(0,len(self.obstacles)): #för varje objekt bollen kan kollidera med
+            print(self.obstacles[i].x)
+            for j in range(self.obstacles[i].width): #för varje x-värde i objektets area
+                print(j)
+                #for n in range(self.obstacles[i].height): #för varje y-värde 
+
+            #if not (((x1-self.x)**2) + (( y1-self.y)**2) <= (self.diameter/2)**2):
+             #   pass
+            #else:
+             #   self.xVelocity = -self.xVelocity
+              #  self.yVelocity = -self.yVelocity
+
+        self.setDirection()
+    
+    def setDirection(self):
+        angle = math.atan(self.yVelocity/self.xVelocity)
+
+class Rectangle:
+    def __init__(self,canvas, x, y, width, height, color, xVelocity, yVelocity):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.xVelocity = xVelocity
+        self.yVelocity = yVelocity
+        self.image = canvas.create_rectangle(x,y,x+width,y+height,fill=color)
     
     def move(self):
 
@@ -38,33 +76,36 @@ def key_handler(event):
 
     if(event.keycode == 87): #w
         print("framåt")
-        ball1.yVelocity = -1
-        ball1.xVelocity = 0.000000001 #får inte vara 0
+        rect1.yVelocity = -1
+        rect2.xVelocity = 0.000000001 #får inte vara 0
     elif(event.keycode == 83): #s
         print("bakåt")
-        ball1.yVelocity = 1
-        ball1.xVelocity = 0.000000001
+        rect1.yVelocity = 1
+        rect1.xVelocity = 0.000000001
     elif(event.keycode == 40): #ner
-        ball2.yVelocity = 1
-        ball2.xVelocity = 0.000000001
+        rect2.yVelocity = 1
+        rect2.xVelocity = 0.000000001
     elif(event.keycode == 38): #upp
-        ball2.yVelocity = -1
+        rect2.yVelocity = -1
         
 
  
 
 root.bind("<Key>", key_handler)   
 
-ball1 = Ball(canvas, 10, 10, 50, "red",1,1)
-ball2 = Ball(canvas, 1000, 10, 50, "blue",1,1)
+rect1 = Rectangle(canvas, 10, 10, 30, 50, "red",1,1)
+rect2 = Rectangle(canvas, 100, 10, 30,50, "blue",1,1)
+players = [rect1, rect2]
+ball1 = Ball(canvas, 300,300, 30, "white", 1,1,players)
 
 while True:
+    rect1.move()
+    rect2.move()
     ball1.move()
-    ball2.move()
     root.after(10)
     root.update()
 
-
+#https://stackoverflow.com/questions/12262017/python-checking-if-coordinates-are-within-circle
 
 root.mainloop()
 

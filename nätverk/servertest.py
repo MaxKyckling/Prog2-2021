7196a5960f6b5f66429bd51d0dbe2cc9edd7fc83
MaxKyckling <maxostlund03@gmail.com>
2022-02-14 15:14:57 +0100

    lite jobb med chatt

--- nätverk/servertest.py
+++ nätverk/servertest.py
@@ -1,24 +1,34 @@
 from socket import *
-
-def start_server():
+def start_server():         # Samma som i förra exemplet
     s = socket()
-    test = s.gethostname()
-    print(test)
-    host = "localhost"
+    host = "localost"
     port = 12345
     s.bind((host, port))
     s.listen()
     return s
-
+def threaded_client(connection):    # Definierar vad tråden ska göra
+    msg = "Servern säger hej till klienten!"
+    connection.send(msg.encode("utf-16"))
+    while True:
+        data = connection.recv(1024)
+        msg = "Servern tog emot följande meddelande: " \
+              + data.decode("utf-16")
+        print(msg)
+        for c in connections:
+            try:
+                    c.send(msg.encode('utf-16'))
+            except: # ifall klienten inte kan nås
+                    connections.remove(c)
+from _thread import *
 s = start_server()
-print("väntar på klient")
-conn, addr = s.accept()
-print("en klient anslöt sig från addressen", addr)
-msg = "Servern säger hej till klienten! Hur mår du idag?"
-b = msg.encode("utf-16")
-conn.send(b)
-print("väntar på meddelande från klienten")
-b = conn.recv(1024)
-msg = b.decode("utf-16")
-print(msg)
-conn.close()
+ThreadCount = 0
+connections = []
+while True: # Skapar en ny tråd för varje klient som ansluter
+    print("Väntar på att en klient ska ansluta till servern...")
+    conn, address = s.accept()
+    print("En ny klient anslöt: " + address[0] + ':'
+          + str(address[1]))
+    start_new_thread(threaded_client, (conn, )) # startar en ny tråd som kör funktionen threaded_client
+    ThreadCount += 1
+    connections.append(conn)
+    print("Tråd nummer: " + str(ThreadCount))
